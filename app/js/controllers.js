17ae00d26be04a3a997f82c41cb980a780c01a08
maksr51314 <maksr51314@gmail.com>
2014-07-12 19:37:28 +0300

    [UI] Added single tabset for page

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,6 +5,17 @@
 angular.module('myApp.controllers', [])
   .controller('MyCtrl1', ['$scope', function($scope) {
 
+        function MyCtrl1() {
+            $scope.tabs = [
+                { title:'Dynamic Title 1', content:'Dynamic content 1' },
+                { title:'Dynamic Title 2', content:'Dynamic content 2' }
+            ];
+
+
+        }
+
+        return new MyCtrl1();
+
   }])
   .controller('MyCtrl2', ['$scope', function($scope) {
 
